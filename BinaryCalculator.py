ab92ccae9d1c2296e7f0ea88ab763d5b05817351
aidan01smith <143528066+aidan01smith@users.noreply.github.com>
2024-02-26 15:14:25 -0500

    BinaryCalculator.py

--- BinaryCalculator.py
+++ BinaryCalculator.py
@@ -1,24 +1,40 @@
 '''def dec_to_bin(dec_num):
     bin_str = format(int(dec_num), 'b')
-    return bin_str'''
-
-'''import tkinter as tk
-
-frame = tk.TK()
-frame.title("Input Box")
-frame.geometry('400x200')'''
+    return bin_str
+#basic binary converter function - maybe turn this into a class?
+print(dec_to_bin(4096))'''
 
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
+# Creates the root window and sets the title and dimensions
 root = tk.Tk()
 root.title('helloworld')
 root.geometry('400x200')
 
+# Sets the frame of the window and makes the child functions
 frame = ttk.Frame(root, padding=10)
 frame.grid()
-ttk.Label(frame, text = "hello world").grid(column = 0, row =0)
-ttk.Button(frame, text = "quit", command = root.destroy).grid(column = 1, row = 0)
+
+# Input box function
+def print_input():
+    inp = inputtxt.get("1.0", "end-1c")
+    lbl.config(text="Provided Input: " + inp)
+
+inputtxt = tk.Text(root, height=5, width=20)
+inputtxt.pack()
+
+# New button creation
+print_button = tk.Button(root, text="print", command=print_input)
+print_button.pack()
+
+# Label creation
+lbl = tk.Label(root, text="")
+lbl.pack()
+
 root.mainloop()
 
+'''#close button child function
+ttk.Button(frame, text = "quit", command = root.destroy).grid(column = 1, row = 0)
+root.mainloop()'''
